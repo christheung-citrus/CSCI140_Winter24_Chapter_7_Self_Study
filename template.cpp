7bc3f12f51df80758c1ab77b231652d9d35f5428
Christopher Theung <CHRTHEUNG000@STUDENT.CITRUSCOLLEGE.EDU>
2024-01-08 09:32:19 -0800

    Initial commit

---
+++ template.cpp
@@ -0,0 +1,14 @@
+// who:  Christopher Theung (ctheung)
+// what: <the function of this program>
+// why:  <the name of the lab>
+// when: <the due date of this lab>
+
+#include <iostream>
+
+int main()
+{
+	// your code goes here
+	
+	
+	return 0;
+}
